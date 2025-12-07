62dd1090d08ac0dd396a54d8201da72b3beaf3f2
Alexander Agudelo <awagud12@gmail.com>
2025-12-07 13:40:10 -0500

    Add comments

--- src/home_cooking/helpers.ts
+++ src/home_cooking/helpers.ts
@@ -1,8 +1,14 @@
+/**
+ * Helper function to calculate next Monday from a given date.
+ * @note If the given date is a Monday, the function will return next week's Monday.
+ * @param fromDate Given date, the function will return the next Monday from the given date.
+ * @returns Next Monday from the given date.
+ */
 export const calculateNextMonday = (fromDate: Date): Date => {
-    const dayOfWeek = fromDate.getDay(); // 0 = Sunday, 1 = Monday, ..., 6 = Saturday
-    const daysToAdd = dayOfWeek === 1 ? 7 : (7 - dayOfWeek); // If Sunday, add 7; otherwise add days to reach next Sunday
+    const dayOfWeek = fromDate.getDay(); 
+    const daysToAdd = dayOfWeek === 1 ? 7 : (7 - dayOfWeek); 
     const nextMonday = new Date(fromDate);
     nextMonday.setDate(fromDate.getDate() + daysToAdd);
-    nextMonday.setHours(0, 0, 0, 0); // Set to start of day
+    nextMonday.setHours(0, 0, 0, 0); 
     return nextMonday;
   };
